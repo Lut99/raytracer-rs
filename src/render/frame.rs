a0709774113aaccd31ad49c4e42e1e97702715f1
Lut99 <mullertim99@gmail.com>
2023-04-29 10:49:37 +0200

    Added normals to the spheres

--- src/render/frame.rs
+++ src/render/frame.rs
@@ -4,7 +4,7 @@
 //  Created:
 //    27 Apr 2023, 14:40:55
 //  Last edited:
-//    29 Apr 2023, 10:36:49
+//    29 Apr 2023, 10:49:12
 //  Auto updated?
 //    Yes
 // 
@@ -26,28 +26,32 @@ use super::generator::RayGenerator;
 
 
 /***** HELPER FUNCTIONS *****/
-/// Computes whether a ray hits a sphere or not.
+/// Computes the hit point of the given ray with the given sphere, if any.
 /// 
 /// # Arguments
 /// - `ray`: The [`Ray`] to check if it hits.
 /// - `sphere`: The [`Sphere`] to compute the hit with.
 /// 
 /// # Returns
-/// true if the [`Ray`] hits, or false otherwise.
-fn hit_sphere(ray: Ray, sphere: &Sphere) -> bool {
+/// The point along the given Ray if there is a hit, or else [None].
+fn hit_sphere(ray: Ray, sphere: &Sphere) -> Option<f64> {
     // Compute the distance between the origin of the ray and the center of the sphere
     let oc: Vec3 = ray.origin - sphere.center;
 
     // We compute `a`, `b` and `c` in the classic ABC-formula. This we do to find the intersections between the Ray (origin + t*direction) and the sphere (x^2 + y^2 + z^2 = r^2).
     // For more explanation, see the tutorial (<https://raytracing.github.io/books/RayTracingInOneWeekend.html#addingasphere/ray-sphereintersection>)
-    let a: f64 = dot3(ray.direct, ray.direct);
-    let b: f64 = 2.0 * dot3(oc, ray.direct);
-    let c: f64 = dot3(oc, oc) - sphere.radius * sphere.radius;
+    let a      : f64 = ray.direct.length2();
+    let half_b : f64 = dot3(oc, ray.direct);
+    let c      : f64 = oc.length2() - sphere.radius * sphere.radius;
 
     // Compute the discriminant only, since we're only interested in the number of roots
     // D < 0 -> no intersection, D == 0 -> one intersection (touching side), D > 0 -> two intersections (passing through)
-    let d: f64 = b*b - 4.0*a*c;
-    d > 0.0
+    let d: f64 = half_b*half_b - a*c;
+    if d >= 0.0 {
+        Some((-half_b - d.sqrt()) / a)
+    } else {
+        None
+    }
 }
 
 
@@ -64,7 +68,11 @@ fn ray_colour(ray: Ray, objects: &[Object]) -> Colour {
     // If it hits the sphere, return the sphere colour
     for obj in objects {
         match obj {
-            Object::Sphere(sphere) => if hit_sphere(ray, sphere) { return Colour::new(1, 0, 0, 1); }
+            Object::Sphere(sphere) => if let Some(point) = hit_sphere(ray, sphere) {
+                // Compute the normal
+                let normal: Vec3 = (ray.at(point) - sphere.center).unit();
+                return 0.5 * Colour::new(normal.x + 1.0, normal.y + 1.0, normal.z + 1.0, 1.0);
+            }
         }
     }
 
