266360e0699a030a23c894ad4c8f0992604f6c57
Lut99 <mullertim99@gmail.com>
2026-06-24 09:57:39 +0200

    Added fuzziness to meterials

--- src/specifications/materials/metal.rs
+++ src/specifications/materials/metal.rs
@@ -10,6 +10,7 @@ use serde::{Deserialize, Serialize};
 use super::Material;
 use crate::math::vec3::dot3;
 use crate::math::{Colour, Ray, Vec3};
+use crate::specifications::materials::random3_uniform;
 use crate::specifications::objects::HitRecord;
 
 
@@ -28,12 +29,21 @@ pub fn reflect(vec: Vec3, norm: Vec3) -> Vec3 { vec - 2.0 * dot3(vec, norm) * no
 pub struct Metal {
     /// The colour of the material.
     pub colour: Colour,
+    /// How much "fuzziness" to apply.
+    ///
+    /// A value of 0 means perfectly reflecting metal, like a mirror.
+    #[serde(default)]
+    pub fuzz:   f64,
 }
 impl Material for Metal {
     #[inline]
     fn scatter(&self, ray: Ray, record: HitRecord) -> (Option<Ray>, Colour) {
         // Compute the scattered ray, making sure the scattered one is not zero
         let reflected: Vec3 = reflect(ray.direct, record.normal);
+        // Add some fuzz by offsetting the endpoint of the reflected vector by a small amount.
+        // This is done by randomly choosing a small vector on a sphere (who's radius is `fuzz`)
+        // and then adding it to the end vector.
+        let reflected: Vec3 = reflected.unit() + self.fuzz * random3_uniform();
 
         // Now we can simply return the new ray to bounce and the colour
         (Some(Ray::new(record.hit, reflected)), self.colour)
